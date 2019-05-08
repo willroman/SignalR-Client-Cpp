e7cb3306ba9e2b6d6775f3d21d3ca85f442808a6
Andrew Stanton-Nurse <andrew@stanton-nurse.com>
2019-05-07 22:07:41 -0700

    Update run-tests.sh

--- run-tests.sh
+++ run-tests.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 config=$1
 test_output_path=$2
+
+# For gcore
+apt-get update
+apt-get install gdb
+
+dumper() {
+  # 1 minute seems to be enough and is less than the timeout
+  sleep 60
+  gcore -o ./signalrclienttests.core $(pidof signalrclienttests)
+}
+
 echo "Running executable"
+dumper &
 if [ "$(uname)" = "Linux" ]; then
   strace -tT "./build.$config/bin/signalrclienttests" --gtest_output=xml:$test_output_path
 else
