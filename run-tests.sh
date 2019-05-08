dd897805f404974bfb1caf3ad648f09fedcae001
Andrew Stanton-Nurse <andrew@stanton-nurse.com>
2019-05-07 19:35:38 -0700

    Update run-tests.sh

--- run-tests.sh
+++ run-tests.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-
+local config=$1
+local test_output_path=$2
 echo "Running executable"
-eval $*
+if [ "$(uname)" = "Linux" ]; then
+  strace "./build.$config/bin/signalrclienttests" --gtest_output=xml:$test_output_path
+else
+  "./build.$config/bin/signalrclienttests" --gtest_output=xml:$test_output_path
+fi
 EXITCODE=$?
 echo "Finished running with exit code: " $EXITCODE
 exit $EXITCODE
