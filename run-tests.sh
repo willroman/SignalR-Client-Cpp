1007f53e42810b3c73b3f825f0350d145e115ba5
Andrew Stanton-Nurse <andrew@stanton-nurse.com>
2019-05-07 20:44:51 -0700

    Update run-tests.sh

--- run-tests.sh
+++ run-tests.sh
@@ -3,7 +3,7 @@ config=$1
 test_output_path=$2
 echo "Running executable"
 if [ "$(uname)" = "Linux" ]; then
-  strace "./build.$config/bin/signalrclienttests" --gtest_output=xml:$test_output_path
+  strace -tT "./build.$config/bin/signalrclienttests" --gtest_output=xml:$test_output_path
 else
   "./build.$config/bin/signalrclienttests" --gtest_output=xml:$test_output_path
 fi
